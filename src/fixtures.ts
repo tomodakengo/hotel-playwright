1615ffde947f33b9c84b2c8f884786af62ab0dc2
tomodakengo <joa.tomoda@gmail.com>
2025-03-09 20:25:19 +0900

    feat: プランページのフィクスチャーを追加
    - PlansPageクラスをフィクスチャーに追加
    - ページオブジェクトの初期化処理を簡潔に実装

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -2,23 +2,19 @@ import { test as base } from '@playwright/test';
 import { HomePage } from '@pages/homePage';
 import { LoginPage } from '@pages/loginPage';
 import { MypagePage } from '@pages/mypagePage';
-
+import { PlansPage } from '@pages/plansPage';
 type Fixtures = {
     homePage: HomePage;
     loginPage: LoginPage;
     mypagePage: MypagePage;
+    plansPage: PlansPage;
 }
 
 export const test = base.extend<Fixtures>({
-    homePage: async ({ page }, use) => {
-        await use(new HomePage(page));
-    },
-    loginPage: async ({ page }, use) => {
-        await use(new LoginPage(page));
-    },
-    mypagePage: async ({ page }, use) => {
-        await use(new MypagePage(page));
-    },
+    homePage: async ({ page }, use) => { await use(new HomePage(page)); },
+    loginPage: async ({ page }, use) => { await use(new LoginPage(page)); },
+    mypagePage: async ({ page }, use) => { await use(new MypagePage(page)); },
+    plansPage: async ({ page }, use) => { await use(new PlansPage(page)); },
 });
 
 export { expect } from '@playwright/test';
