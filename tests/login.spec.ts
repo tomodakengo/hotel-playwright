3a066c3610cf03489582446a2832c5acf574d7a3
tomodakengo <joa.tomoda@gmail.com>
2025-03-09 19:47:56 +0900

    test: ログインテストを複数ユーザーで検証
    - ログインテストを複数のユーザーで実行するよう変更
    - テスト前後処理を追加（ログイン後のログアウト）
    - ページオブジェクトを活用したログインテストに更新

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,30 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@fixtures';
+
+const loginUsers = [
+    {
+        email: 'ichiro@example.com',
+        password: 'password'
+    },
+    {
+        email: 'sakura@example.com',
+        password: 'pass1234'
+    },
+];
 
 test.describe('ログイン機能', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('https://hotel-example-site.takeyaqa.dev/ja/');
+        await page.goto('/ja/');
     });
 
-    test('ログイン成功', async ({ page }) => {
+    test.afterEach(async ({ mypagePage }) => {
+        await mypagePage.clickHeaderMenu('ログアウト');
+    });
 
-
+    for (const user of loginUsers) {
+        test(`ログイン成功: ${user.email}`, async ({ homePage, loginPage, mypagePage }) => {
+            await homePage.clickHeaderMenu('ログイン');
+            await loginPage.login(user.email, user.password);
+            await mypagePage.assertLoginSuccess();
+        });
+    }
 });
