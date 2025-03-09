8c3b688c07753724fd67c6ff6cfb78e28d87b5bf
tomodakengo <joa.tomoda@gmail.com>
2025-03-09 19:44:29 +0900

    feat: ヘッダーメニューのメニューリストを認証状態に応じて分割

--- types/headerMenu.d.ts
+++ types/headerMenu.d.ts
@@ -1,1 +1,4 @@
-type headerMenuList = "ホーム" | "宿泊予約" | "会員登録" | "マイページ" | "ログイン" | "ログアウト";
+type headerMenuListWithAuth = "ホーム" | "宿泊予約" | "マイページ" | "ログアウト";
+type headerMenuListWithoutAuth = "ホーム" | "宿泊予約" | "会員登録" | "ログイン";
+
+type headerMenuList = headerMenuListWithAuth | headerMenuListWithoutAuth;
